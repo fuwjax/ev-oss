70441dc53471204c306f5ff45e13880a36b45d02
Michael Doberenz <fuwjax@gmail.com>
2015-01-16 11:16:12 -0600

    added Streams.stream

--- funco/src/main/java/org/echovantage/util/Streams.java
+++ funco/src/main/java/org/echovantage/util/Streams.java
@@ -1,5 +1,7 @@
 package org.echovantage.util;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.stream.Stream;
 
 /**
@@ -8,5 +10,9 @@ import java.util.stream.Stream;
 public class Streams {
     public static <T> Iterable<T> over(Stream<T> stream){
         return stream::iterator;
+    }
+
+    public static <T> Stream<T> stream(Collection<T> c){
+        return c == null ? Collections.<T>emptySet().stream() : c.stream();
     }
 }
