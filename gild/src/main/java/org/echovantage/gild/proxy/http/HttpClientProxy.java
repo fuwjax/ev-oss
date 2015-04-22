160e74d3455744e8c5761cf145b78eb4f676e2b3
Michael Doberenz <fuwjax@gmail.com>
2015-04-22 13:26:37 -0500

    added canonical flag for client proxy

--- gild/src/main/java/org/echovantage/gild/proxy/http/HttpClientProxy.java
+++ gild/src/main/java/org/echovantage/gild/proxy/http/HttpClientProxy.java
@@ -45,117 +45,118 @@ import org.echovantage.gild.proxy.AbstractServiceProxy;
 import org.echovantage.util.io.ReadOnlyPath;
 
 public class HttpClientProxy extends AbstractServiceProxy {
-    private static final Pattern REQUEST_LINE_PATTERN = Pattern.compile("^(?<method>[\\S]+)\\s(?<path>[\\S]+)\\s(?<version>[\\S]+)");
+	private static final Pattern REQUEST_LINE_PATTERN = Pattern.compile("^(?<method>[\\S]+)\\s(?<path>[\\S]+)\\s(?<version>[\\S]+)");
+	private final String host;
+	private final int port;
+	private final boolean canonical;
 
-    private final String host;
-    private final int port;
+	public HttpClientProxy(final int port) {
+		this("localhost", port);
+	}
 
-    public HttpClientProxy(final int port) {
-        this("localhost", port);
-    }
+	public HttpClientProxy(final String host, final int port) {
+		this(host, port, true);
+	}
 
-    public HttpClientProxy(final String host, final int port) {
-        this.host = host;
-        this.port = port;
-    }
+	public HttpClientProxy(final String host, final int port, final boolean canonical) {
+		this.host = host;
+		this.port = port;
+		this.canonical = canonical;
+	}
 
-    public void send() {
-        configured();
-    }
+	public void send() {
+		configured();
+	}
 
-    @Override
-    protected boolean preserveImpl(Path output, ReadOnlyPath golden) throws Exception {
-        // preserved in prepare
-        return false;
-    }
+	@Override
+	protected boolean preserveImpl(final Path output, final ReadOnlyPath golden) throws Exception {
+		// preserved in prepare
+		return false;
+	}
 
-    @Override
-    protected void prepareImpl(ReadOnlyPath input, Path output) throws Exception {
-        Files.createDirectories(output);
-        List<ReadOnlyPath> paths = new ArrayList<>();
-        try (DirectoryStream<ReadOnlyPath> stream = input.newDirectoryStream()) {
-            for (ReadOnlyPath test : stream) {
-                paths.add(test);
-            }
-        }
-        Collections.sort(paths);
-        for (final ReadOnlyPath file : paths) {
-            HttpRequest request = buildRequest(file);
-            HttpConnection connection = new SocketHttpConnection(new Socket(host, port));
-            request.open(connection);
-            HttpResponse response = request.send();
-            persistResponse(response, output.resolve(file.getFileName()));
-        }
-    }
+	@Override
+	protected void prepareImpl(final ReadOnlyPath input, final Path output) throws Exception {
+		Files.createDirectories(output);
+		final List<ReadOnlyPath> paths = new ArrayList<>();
+		try(DirectoryStream<ReadOnlyPath> stream = input.newDirectoryStream()) {
+			for(final ReadOnlyPath test : stream) {
+				paths.add(test);
+			}
+		}
+		Collections.sort(paths);
+		for(final ReadOnlyPath file : paths) {
+			final HttpRequest request = buildRequest(file);
+			final HttpConnection connection = new SocketHttpConnection(new Socket(host, port));
+			request.open(connection);
+			final HttpResponse response = request.send();
+			persistResponse(response, output.resolve(file.getFileName()));
+		}
+	}
 
-    private void persistResponse(HttpResponse response, Path output) throws IOException {
-        try (BufferedWriter w = newBufferedWriter(output, ISO_8859_1)) {
-            w.append(response.httpVersion()).append(' ').append(Integer.toString(response.statusCode())).append(' ').append(response.statusPhrase())
-                    .append('\n');
+	private void persistResponse(final HttpResponse response, final Path output) throws IOException {
+		try(BufferedWriter w = newBufferedWriter(output, ISO_8859_1)) {
+			w.append(response.httpVersion()).append(' ').append(Integer.toString(response.statusCode())).append(' ').append(response.statusPhrase())
+			      .append('\n');
+			final Map<String, String[]> sortedHeaders = canonical ? new TreeMap<>(response.headers()) : response.headers();
+			for(final Map.Entry<String, String[]> entry : sortedHeaders.entrySet()) {
+				final String headerName = canonical ? entry.getKey().toLowerCase() : entry.getKey();
+				if(headerName.equals("date")) {
+					w.append("date: ${DATE}\n");
+				} else {
+					for(final String value : entry.getValue()) {
+						w.append(headerName).append(": ").append(value).append('\n');
+					}
+				}
+			}
+			w.append('\n');
+			w.append(response.body());
+		}
+	}
 
-            TreeMap<String, String[]> sortedHeaders = new TreeMap<>(response.headers());
-            for (Map.Entry<String, String[]> entry : sortedHeaders.entrySet()) {
-                String headerName = entry.getKey().toLowerCase();
-                if (headerName.equals("date")) {
-                    w.append("date: ${DATE}\n");
-                } else {
-                    for (String value : entry.getValue()) {
-                        w.append(headerName).append(": ").append(value).append('\n');
-                    }
-                }
-            }
+	private HttpRequest buildRequest(final ReadOnlyPath file) throws IOException {
+		try(InputStream is = file.newInputStream(); Reader r = new InputStreamReader(is); BufferedReader br = new BufferedReader(r)) {
+			final HttpRequest request = new HttpRequest();
+			request.removeHeader("Connection");
+			String line = br.readLine();
+			parseRequestLine(line, request);
+			while((line = br.readLine()) != null) {
+				if(line.isEmpty()) {
+					break;
+				}
+				parseHeader(line, request);
+			}
+			appendBody(br, request);
+			return request;
+		}
+	}
 
-            w.append('\n');
-            w.append(response.body());
-        }
-    }
+	private void appendBody(final BufferedReader br, final HttpRequest request) throws IOException {
+		final StringBuilder body = new StringBuilder();
+		final char[] cbuf = new char[4096];
+		int numChars;
+		while(true) {
+			numChars = br.read(cbuf);
+			if(numChars > -1) {
+				body.append(cbuf, 0, numChars);
+			} else {
+				break;
+			}
+		}
+		request.body(body.toString());
+	}
 
-    private HttpRequest buildRequest(ReadOnlyPath file) throws IOException {
-        try (InputStream is = file.newInputStream(); Reader r = new InputStreamReader(is); BufferedReader br = new BufferedReader(r)) {
-            HttpRequest request = new HttpRequest();
-            request.removeHeader("Connection");
-            String line = br.readLine();
+	private void parseHeader(final String line, final HttpRequest request) {
+		final String[] header = line.split(":");
+		request.header(header[0], header[1]);
+	}
 
-            parseRequestLine(line, request);
-            while ((line = br.readLine()) != null) {
-                if (line.isEmpty()) {
-                    break;
-                }
-                parseHeader(line, request);
-            }
-            appendBody(br, request);
-            return request;
-        }
-    }
-
-    private void appendBody(BufferedReader br, HttpRequest request) throws IOException {
-        StringBuilder body = new StringBuilder();
-        char[] cbuf = new char[4096];
-        int numChars;
-        while (true) {
-            numChars = br.read(cbuf);
-            if (numChars > -1) {
-                body.append(cbuf, 0, numChars);
-            } else {
-                break;
-            }
-        }
-        request.body(body.toString());
-    }
-
-    private void parseHeader(String line, HttpRequest request) {
-        String[] header = line.split(":");
-        request.header(header[0], header[1]);
-    }
-
-    private void parseRequestLine(String line, HttpRequest request) {
-        Matcher m = REQUEST_LINE_PATTERN.matcher(line);
-        if (!m.matches()) {
-            throw new IllegalArgumentException("Request line must be formatted as 'METHOD URI VERSION'. was '" + line + "'");
-        }
-        request.method(m.group("method"));
-        request.path(m.group("path"));
-        request.httpVersion(m.group("version"));
-    }
-
+	private void parseRequestLine(final String line, final HttpRequest request) {
+		final Matcher m = REQUEST_LINE_PATTERN.matcher(line);
+		if(!m.matches()) {
+			throw new IllegalArgumentException("Request line must be formatted as 'METHOD URI VERSION'. was '" + line + "'");
+		}
+		request.method(m.group("method"));
+		request.path(m.group("path"));
+		request.httpVersion(m.group("version"));
+	}
 }
